6dc32c3d1bfb1ef03686c36dad02f93329e48870
haikalnuril <haikal.nuril23@gmail.com>
2024-07-24 15:41:53 +0700

    feat: template react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center min-h-screen'>
+        <div>Hello World!</div>
+      </div>
+    </>
+  )
+}
+
+export default App
