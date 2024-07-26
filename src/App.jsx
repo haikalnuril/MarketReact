96f4efbe43730e64bcbc0c9ed731a5904bc9c83b
haikalnuril <haikal.nuril23@gmail.com>
2024-07-26 23:29:39 +0700

    feat: login

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ function App() {
   return (
     <>
       <div className='flex justify-center items-center min-h-screen'>
-        <div>Hello World!</div>
+        <div className="w-full max-w-xs">
+          <h1 className="text-4xl font-bold mb-2 text-blue-600">Login</h1>
+          <p className="font-medium text-slate-500">
+            Welcome, PLease enter your details
+          </p>
+          <form action="">
+            <div className="mb-6">
+              <label htmlFor="" className="block text-slate-700 text-sm font-bold mb-2">
+                Email
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
