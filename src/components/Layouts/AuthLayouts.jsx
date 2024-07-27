f4899dd651f716e916ebcb511c61754791873cf0
haikalnuril <haikal.nuril23@gmail.com>
2024-07-27 16:33:36 +0700

    feat: conditional state

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
+
 const AuthLayout = (props) => {
-    const {children, title} = props;
+    const {children, title, type} = props;
     return (
         <div className='flex justify-center items-center min-h-screen'>
         <div className="w-full max-w-xs">
@@ -8,6 +10,28 @@ const AuthLayout = (props) => {
             Welcome, PLease enter your details
           </p>
           {children}
+            <p 
+                className="text-sm mt-5 text-center"
+            >
+                {type === "login" ? "Don't have an account? " : "Already have an account? "}
+                
+                {type === "login" &&(
+                    <Link 
+                        to="/register" 
+                        className="font-bold text-blue-600"
+                    >
+                        Sign Up
+                    </Link>
+                )}
+                {type === "register" &&(
+                    <Link 
+                        to="/login" 
+                        className="font-bold text-blue-600"
+                    >
+                        Sign In
+                    </Link>
+                )}
+            </p>
         </div>
       </div>
     )
