7a3c8c3e41b762922f8bc4c1b2cd306d0d4c4d29
haikalnuril <haikal.nuril23@gmail.com>
2024-07-29 13:04:45 +0700

    feat: listing products

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,28 +1,47 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        price: "Rp. 1.000.000",
+        image: "/images/shoes.jpg",
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quas quia ab, ratione aliquid quibusdam commodi quos magni explicabo corrupti impedit fugit nisi eius ullam quod, voluptatum amet sunt placeat.`
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        price: "Rp. 500.000",
+        image: "/images/shoes.jpg",
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quas quia ab.`
+    },
+    {
+        id: 3,
+        name: "Sepatu Nice",
+        price: "Rp. 1.500.000",
+        image: "/images/shoes.jpg",
+        description: `Ini adalah sepatu dari brand Nice.`
+    }
+]
+
 const ProductPage = () => {
 
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header images="/images/shoes.jpg"/>
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quas quia ab, ratione aliquid quibusdam commodi quos magni explicabo corrupti impedit fugit nisi eius ullam quod, voluptatum amet sunt placeat.
-                </CardProduct.Body>
-                <CardProduct.Footer
-                price = "Rp. 1.000.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header images="/images/shoes.jpg"/>
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic quas quia ab, ratione aliquid quibusdam commodi quos magni explicabo corrupti impedit fugit nisi eius ullam quod, voluptatum amet sunt placeat.
-                </CardProduct.Body>
-                <CardProduct.Footer
-                price = "Rp. 1.000.000" />
-            </CardProduct>
+            {products.map((product) => {
+                return(
+                    <CardProduct>
+                        <CardProduct.Header images={product.image}/>
+                        <CardProduct.Body name={product.name}>
+                            {product.description}
+                        </CardProduct.Body>
+                        <CardProduct.Footer price={product.price} />
+                    </CardProduct>
+                    );
+            })}
         </div>
     );
-}
+};
 
 export default ProductPage;
