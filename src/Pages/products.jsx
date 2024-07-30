2282a9a8569fe085080c3ac59ec2e4a78c05b11e
haikalnuril <haikal.nuril23@gmail.com>
2024-07-30 15:46:25 +0700

    feat: total price

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 
@@ -29,12 +29,22 @@ const products = [
 const email = localStorage.getItem('email');
 
 const ProductPage = () => {
-    const [cart, setCart] = useState([
-        {
-            id: 1,
-            qty: 1,
+    const [cart, setCart] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem('cart')) || []);
+    }, []);
+
+    useEffect(() => {
+        if(cart.length > 0) {
+            const sum = cart.reduce((acc, item)=>{
+                const product = products.find((product) => product.id === item.id);
+                return acc + product.price * item.qty;
+            }, 0);
+            setTotalPrice(sum);
+            localStorage.setItem('cart', JSON.stringify(cart));
         }
-    ]);
+    }, [cart]);
 
     const handleLogout = () => {
         localStorage.removeItem('email');
@@ -100,6 +110,10 @@ const ProductPage = () => {
                                     </tr>
                                 )
                             })}
+                            <tr>
+                                <td colSpan={3}><b>Total Price</b></td>
+                                <td><b>Rp. {(totalPrice).toLocaleString('id-ID', {styles: 'currency', currency: 'IDR'})}</b></td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
